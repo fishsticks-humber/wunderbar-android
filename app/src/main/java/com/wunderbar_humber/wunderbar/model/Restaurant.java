ab51696c2dc80f1a54b3b38a60a3d588c875cc2d
Mohit Deshpande <mohitdeshpande@icloud.com>
2017-12-19 12:28:24 -0500

    create restaurant model and fragment adapters

---
+++ app/src/main/java/com/wunderbar_humber/wunderbar/model/Restaurant.java
@@ -0,0 +1,56 @@
+package com.wunderbar_humber.wunderbar.model;
+
+import android.media.Image;
+
+/**
+ * Created by mohit on 2017-12-19.
+ */
+
+public class Restaurant {
+
+    private String id;
+    private String name;
+    private Image image;
+    private String address;
+    private Float rating;
+
+    public Float getRating() {
+        return rating;
+    }
+
+    public void setRating(Float rating) {
+        this.rating = rating;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public void setImage(Image image) {
+        this.image = image;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
