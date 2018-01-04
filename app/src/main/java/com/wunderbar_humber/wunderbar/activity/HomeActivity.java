0da44f9c13c1ff910d1aa3731c7eacf50a015c5a
Mohit Deshpande <mohitdeshpande@icloud.com>
2018-01-04 00:00:05 -0500

    add choose on map button

--- app/src/main/java/com/wunderbar_humber/wunderbar/activity/HomeActivity.java
+++ app/src/main/java/com/wunderbar_humber/wunderbar/activity/HomeActivity.java
@@ -164,13 +164,11 @@ public class HomeActivity extends AppCompatActivity
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.filter_restaurants) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.filter_nightlife) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
+        } else if (id == R.id.map_location_choose) {
 
         } else if (id == R.id.nav_share) {
 
