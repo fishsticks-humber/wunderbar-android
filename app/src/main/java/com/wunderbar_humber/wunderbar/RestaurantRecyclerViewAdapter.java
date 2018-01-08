6a0565e70b505ebb1bb1d9013926fe1abab6f0f9
Maxim Shishkov <ceppersx@gmail.com>
2018-01-08 13:51:13 -0500

    getting reviews for restaurants

--- app/src/main/java/com/wunderbar_humber/wunderbar/RestaurantRecyclerViewAdapter.java
+++ app/src/main/java/com/wunderbar_humber/wunderbar/RestaurantRecyclerViewAdapter.java
@@ -2,21 +2,25 @@ package com.wunderbar_humber.wunderbar;
 
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
 import com.wunderbar_humber.wunderbar.RestaurantFragment.OnListFragmentInteractionListener;
+import com.wunderbar_humber.wunderbar.activity.MapActivity;
 import com.wunderbar_humber.wunderbar.activity.RestaurantActivity;
 import com.wunderbar_humber.wunderbar.model.HomeModel;
 import com.yelp.fusion.client.models.Business;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.net.URLEncoder;
 import java.util.List;
 
 /**
@@ -29,6 +33,7 @@ public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<Restaura
     private final OnListFragmentInteractionListener mListener;
     private final Context context;
     private List<Business> mValues;
+
 
     public RestaurantRecyclerViewAdapter(HomeModel model, OnListFragmentInteractionListener listener, Context context) {
         mValues = model.getBusinessList();
@@ -55,11 +60,13 @@ public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<Restaura
 
     @Override
     public void onBindViewHolder(final ViewHolder holder, final int position) {
-        holder.mItem = mValues.get(position);
-        holder.mIdView.setText(mValues.get(position).getName());
-        holder.mContentView.setText(mValues.get(position).getPrice());
+        final Business business = mValues.get(position);
 
-        String imageUrl = mValues.get(position).getImageUrl();
+        holder.mItem = business;
+        holder.mIdView.setText(business.getName());
+        holder.mContentView.setText(business.getPrice());
+
+        String imageUrl = business.getImageUrl();
         if (StringUtils.isNotEmpty(imageUrl)) {
             Picasso.with(context).load(imageUrl).into(holder.imageView);
         }
@@ -68,10 +75,39 @@ public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<Restaura
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(view.getContext(), RestaurantActivity.class);
-                intent.putExtra("restaurantId", mValues.get(position).getId());
+                intent.putExtra("restaurantId", business.getId());
                 view.getContext().startActivity(intent);
             }
         });
+
+        holder.mapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(view.getContext(), MapActivity.class);
+                intent.putExtra("latitude", business.getCoordinates().getLatitude());
+                intent.putExtra("longitude", business.getCoordinates().getLongitude());
+                intent.putExtra("restaurant", business.getName());
+                view.getContext().startActivity(intent);
+            }
+        });
+
+        holder.uberButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String uberUrl = "https://m.uber.com/ul/"
+                        + "?client_id=HAb_WWbX4pVCH7Ugd8FuLHnhRR0cnSEQ"
+                        + "&action=setPickup"
+                        + "&pickup=my_location"
+                        + "&dropoff[latitude]=" + business.getCoordinates().getLatitude()
+                        + "&dropoff[longitude]=" + business.getCoordinates().getLongitude()
+                        + "&dropoff[nickname]=" + URLEncoder.encode(business.getName());
+
+                Uri uri = Uri.parse(uberUrl);
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                context.startActivity(intent);
+            }
+        });
+
     }
 
     @Override
@@ -84,6 +120,9 @@ public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<Restaura
         public final TextView mIdView;
         public final TextView mContentView;
         public final ImageView imageView;
+        public final Button uberButton;
+        public final Button mapButton;
+
         public Business mItem;
 
         public ViewHolder(View view) {
@@ -92,7 +131,10 @@ public class RestaurantRecyclerViewAdapter extends RecyclerView.Adapter<Restaura
             mIdView = view.findViewById(R.id.id);
             mContentView = view.findViewById(R.id.content);
             imageView = view.findViewById(R.id.restaurantImage);
+            uberButton = view.findViewById(R.id.uberButton);
+            mapButton = view.findViewById(R.id.mapsButton);
         }
+
 
         @Override
         public String toString() {
